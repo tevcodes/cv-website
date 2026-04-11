60b9824082434161d8f4f921fd743534b271d9a0
tevcodes <monkaeweb@gmail.com>
2026-04-11 12:30:14 +0200

    CV Update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@
 
 import "./style.css";
 
+const menu = document.querySelector('#mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active');
+  navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    menu.classList.remove("is-active");
+  });
+});
+
 document.querySelectorAll("button[data-url]").forEach((button) => {
   button.addEventListener("click", () => {
     const url = button.dataset.url;
